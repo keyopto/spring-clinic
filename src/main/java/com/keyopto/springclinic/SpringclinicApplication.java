70b21e0d9b4de1a1884d040f51cfe9e6ccf9b33b
keyopto <keyopto@gmail.com>
2025-05-14 14:26:55 +0200

    feat: init

---
+++ src/main/java/com/keyopto/springclinic/SpringclinicApplication.java
@@ -0,0 +1,13 @@
+package com.keyopto.springclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyopto/springclinic/SpringclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyopto.springclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
