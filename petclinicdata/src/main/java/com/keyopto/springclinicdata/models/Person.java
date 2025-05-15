3c71c3b45e6540db03b4babbcc13f77983d2bf2e
keyopto <keyopto@gmail.com>
2025-05-15 14:22:49 +0200

    feat: add id to Pet and Person

--- petclinicdata/src/main/java/com/keyopto/springclinicdata/models/Person.java
+++ petclinicdata/src/main/java/com/keyopto/springclinicdata/models/Person.java
@@ -2,12 +2,21 @@ package com.keyopto.springclinicdata.models;
 
 public abstract class Person {
 
+  private Long id;
   private String firstName;
   private String lastName;
 
   public Person(String firstName, String lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
   }
 
   public String getFirstName() {
--- petclinicdata/src/main/java/com/keyopto/springclinicdata/models/Pet.java
+++ petclinicdata/src/main/java/com/keyopto/springclinicdata/models/Pet.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 public class Pet {
 
+  private Long id;
   private PetType type;
   private Owner owner;
   private LocalDate birthDate;
@@ -13,6 +14,14 @@ public class Pet {
     this.type = type;
     this.owner = owner;
     this.birthDate = birthDate;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
   }
 
   public PetType getType() {
