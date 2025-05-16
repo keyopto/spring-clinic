15e6ba57ba21589dd4ee18b3b0688599747e0acd
keyopto <keyopto@gmail.com>
2025-05-16 11:37:25 +0200

    feat: add owner controller and template

--- petclinicweb/src/main/java/com/keyopto/springclinicweb/controllers/OwnerController.java
+++ petclinicweb/src/main/java/com/keyopto/springclinicweb/controllers/OwnerController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/vets")
-public class VetController {
+@RequestMapping("owners")
+public class OwnerController {
 
   @RequestMapping({"", "/"})
-  String getList() {
-    return "vets/index";
+  public String getList() {
+    return "owners/index";
   }
 }
