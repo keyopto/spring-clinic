72ba9a2734fa1d1a4639831ecce242884fd498d8
keyopto <keyopto@gmail.com>
2025-05-19 13:34:48 +0200

    feat: add vet list page

--- petclinicweb/src/main/java/com/keyopto/springclinic/controllers/VetController.java
+++ petclinicweb/src/main/java/com/keyopto/springclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package com.keyopto.springclinic.controllers;
 
+import com.keyopto.springclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+  private final VetService vetService;
+
+  public VetController(VetService vetService) {
+    this.vetService = vetService;
+  }
+
   @RequestMapping({"", "/"})
-  String getList() {
+  String getList(Model model) {
+    model.addAttribute("vets", this.vetService.findAll());
     return "vets/index";
   }
 }
